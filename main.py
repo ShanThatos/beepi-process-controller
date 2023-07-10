f8975070a2197f2c61e257ceb6f0cc68a46d6120
ShanThatos <shanthkoka@gmail.com>
2023-07-10 18:50:41 -0500

    make-push-update

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def index():
     return render_template("index.html")
 
 
-with open("./ouch.txt", "w") as f:
-    f.write("hoooo")
+# with open("./ouch.txt", "w") as f:
+#     f.write("hoooo")
 
 
 app.run("0.0.0.0", 8000, debug=True)
