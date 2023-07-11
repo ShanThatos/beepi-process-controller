6d7743a6fd37cb551fe58532023e3c16a4fbbc3f
ShanThatos <shanthkoka@gmail.com>
2023-07-11 00:06:28 -0500

    make-push-update

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from subprocess import Popen
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from waitress import serve
 
 app = Flask(__name__)
+
+@app.before_request
+def before_request():
+    print(f"[{request.method} REQUEST] from {request.remote_addr}\n\t{request.url}")
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
+
 if __name__ == "__main__":
-    # cloudflared tunnel run --url localhost:8000 beepi.shanthatos.dev
-    Popen(["cloudflared", "tunnel", "run", "--url", "localhost:8000", "beepi.shanthatos.dev"])
+    # cloudflared tunnel run --url 0.0.0.0:8000 beepi.shanthatos.dev
+    Popen(["cloudflared", "tunnel", "run", "--url", "0.0.0.0:8000", "beepi.shanthatos.dev"])
 
     # app.run("0.0.0.0", 8000, debug=True)
-    serve(app, listen="localhost:8000")
+    serve(app, listen="0.0.0.0:8000", threads=1)
