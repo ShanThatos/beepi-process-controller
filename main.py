911ceb45263688da2dfec0c18474e818f8f61b18
ShanThatos <shanthkoka@gmail.com>
2023-07-10 23:37:11 -0500

    make-push-update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from subprocess import Popen
 from flask import Flask, render_template
 from waitress import serve
 
@@ -9,5 +10,8 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
+    # cloudflared tunnel run --url localhost:8000 beepi.shanthatos.dev
+    Popen(["cloudflared", "tunnel", "run", "--url", "localhost:8000", "beepi.shanthatos.dev"])
+
     # app.run("0.0.0.0", 8000, debug=True)
     serve(app, listen="localhost:8000")
